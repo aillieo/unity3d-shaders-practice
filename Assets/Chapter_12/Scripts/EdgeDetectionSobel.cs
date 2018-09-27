711c8e76316997565fc17bf74420d3b94c546dfa
aillieo <aillieo@163.com>
2018-09-27 22:40:56 +0800

    update

---
+++ Assets/Chapter_12/Scripts/EdgeDetectionSobel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EdgeDetectionSobel : OnRenderImageBase
+{
+
+    [Range(0.0f, 1.0f)]
+    public float edgesOnly = 0.0f;
+    public Color edgeColor = Color.black;
+    public Color backgroundColor = Color.white;
+
+    protected override void ProcessMat(Material mat)
+    {
+        mat.SetFloat("_EdgeOnly", edgesOnly);
+        mat.SetColor("_EdgeColor", edgeColor);
+        mat.SetColor("_BackgroundColor", backgroundColor);
+    }
+}
